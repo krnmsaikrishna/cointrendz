b4b2dceca48c9ca7329df1013cce1307602d13d1
KrnmSaiKrishna <38278654+krnmsaikrishna@users.noreply.github.com>
2022-03-22 20:51:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,13 +81,12 @@ def my_handler(client, message):
     _t = _t.splitlines()
     
     data = {}
-    data['coin'] = _o(_t[0],0).split('/')[0]
-    data['pair'] = _o(_t[0],0).split('/')[1]
-    data['type'] = _o(_t[1],2)
-    data['volu'] = _o(_t[2],0)
-    data['price'] = _o(_t[3],1)
-    data['size'] = _o(_t[4],2)
-    data['day'] = _o(_t[6],2)
+    data['coin'] = _o(_t[0],2).split('/')[0]
+    data['pair'] = _o(_t[0],2).split('/')[1]
+    data['price'] = _o(_t[1],1)
+    data['change'] = _o(_t[1],3)
+    data['vol'] = _o(_t[2],1)
+    data['volup'] = _o(_t[2],3)
     data = json.dumps(data)
 
     req = requests.get(site_url+"pump&data="+data)
