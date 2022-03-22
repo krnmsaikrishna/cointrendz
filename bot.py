e641510884b7b75b95ac04d99a2f8a868923af76
KrnmSaiKrishna <38278654+krnmsaikrishna@users.noreply.github.com>
2022-03-22 19:15:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import base64
 import json
 import re
+import requests 
 from pyrogram import Client, filters
 from pyrogram.methods.messages import edit_inline_media
 #from pyrogram.types import User, Message
@@ -16,9 +17,81 @@ app = Client(
     api_hash  = "2bd06ae25625234f0316b7a8e193bb6a"
     )
 
-channel_1 = "cointrendz_whalehunter" 
+channel_1 = "cointrendz_whalehunter"
+channel_2 = "cointrendz_wallmonitor"
+channel_3 = "cointrendz_pumpdetector"
+
+site_url = "https://mysgpa.me/coin/tel.php?type="
+
+
+def _o(str,pos):
+    return str.split(' ')[pos]
+
 
 @app.on_message(filters.channel  &  filters.create(lambda _,__,query: query.chat.username == channel_1))
 def my_handler(client, message):
-    print(message)
+    
+    _t = message.text 
+    _t = _t.splitlines()
+    
+    data = {}
+    data['coin'] = _o(_t[0],0).split('/')[0]
+    data['pair'] = _o(_t[0],0).split('/')[1]
+    data['type'] = _o(_t[1],2)
+    data['volu'] = _o(_t[2],0)
+    data['price'] = _o(_t[3],1)
+    data['size'] = _o(_t[4],2)
+    data['day'] = _o(_t[6],2)
+    data = json.dumps(data)
+
+    req = requests.get(site_url+"spot&data="+data)
+    print(req.text)
+    
+
+
+
+
+
+@app.on_message(filters.channel  &  filters.create(lambda _,__,query: query.chat.username == channel_2))
+def my_handler(client, message):
+    
+    _t = message.text 
+    _t = _t.splitlines()
+    
+    data = {}
+    data['coin'] = _o(_t[0],0).split('/')[0]
+    data['pair'] = _o(_t[0],0).split('/')[1]
+    data['type'] = _o(_t[1],0)
+    data['volu'] = _o(_t[2],0)
+    data['price'] = _o(_t[2],3)
+    data['size'] = _o(_t[3],2)
+    data['day'] = _o(_t[4],2)
+    data = json.dumps(data)
+
+    req = requests.get(site_url+"wall&data="+data)
+    print(req.text)
+
+
+
+
+@app.on_message(filters.channel  &  filters.create(lambda _,__,query: query.chat.username == channel_3))
+def my_handler(client, message):
+    
+    _t = message.text 
+    _t = _t.splitlines()
+    
+    data = {}
+    data['coin'] = _o(_t[0],0).split('/')[0]
+    data['pair'] = _o(_t[0],0).split('/')[1]
+    data['type'] = _o(_t[1],2)
+    data['volu'] = _o(_t[2],0)
+    data['price'] = _o(_t[3],1)
+    data['size'] = _o(_t[4],2)
+    data['day'] = _o(_t[6],2)
+    data = json.dumps(data)
+
+    req = requests.get(site_url+"pump&data="+data)
+    print(req.text)
+    
+    
 app.run()
