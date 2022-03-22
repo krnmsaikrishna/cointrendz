efa9e59912a26a32572ebe97b1487cecd96164a3
KrnmSaiKrishna <38278654+krnmsaikrishna@users.noreply.github.com>
2022-03-22 17:31:48 +0530

    Add files via upload

---
+++ pyro.py
@@ -0,0 +1,28 @@
+import base64
+import json
+import re
+from pyrogram import Client, filters
+from pyrogram.methods.messages import edit_inline_media
+#from pyrogram.types import User, Message
+#from pyrogram.types import ChatPermissions
+
+#  bot_token = "5020987340:AAFRWHMCTiXZNMnOdFrtH1znJ-WGvc_uXaA"
+
+app = Client(
+    "boss",
+    api_id    = 8793184,
+    api_hash  = "2bd06ae25625234f0316b7a8e193bb6a"
+    )
+
+
+with app:
+    app.join_chat("nsim_channel")
+
+
+@app.on_message(filters.channel)
+def my_handler(client, message):
+    print(message)
+
+
+
+app.run()
