620f1963f457db34a68f482c8502e8597dbd3f9d
KrnmSaiKrishna <38278654+krnmsaikrishna@users.noreply.github.com>
2022-03-26 07:51:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,30 +18,25 @@ channel_3 = "cointrendz_pumpdetector"
 site_url = "http://cointrendz.ml/bot.php?type="
 
 
-def _o(str,pos):
-    return str.split(' ')[pos]
-
-
 @app.on_message(filters.channel  &  filters.create(lambda _,__,query: query.chat.username == channel_1))
 def my_handler(client, message):
   try:
     _t = message.text 
     _t = _t.splitlines()
     data = {}
-    stam = datetime.fromtimestamp(message.date).strftime('%I:%M %p')
-    data['stam']  = stam
-    data['coin']  = _t[0].split(' ')[0]
+    data['coin']  = (_t[0].split(' ')[0]).split('/')[0]
+    data['pair']  = (_t[0].split(' ')[0]).split('/')[1]
     data['type']  = _t[1].split(' ')[2]
     data['volu']  = _t[2].split(' ')[0]
-    data['prch']  = (_t[3].split(' ')[3])[1:-1]
-    data['price'] = ' '.join(_t[3].split(' ')[1:3])
-    data['size']  = ' '.join(_t[4].split(' ')[2:4])
-    data['24h']   = _t[6].split(':')[1]
+    data['prch']  = (_t[3].split(' ')[3])[1:-2]
+    data['price'] = _t[3].split(' ')[1]
+    data['size']  = _t[4].split(' ')[2]
+    data['24h']   = _t[6].split(' ')[2]
     data = json.dumps(data)
     req = requests.get(site_url+"spot&data="+data)
     print(req.text)
-  except:
-    print("Error on Spot")
+  except Exception as e:
+    print("Error :: Spot Module ::"+e.message)
 
 
 
@@ -53,19 +48,18 @@ def my_handler(client, message):
     _t = message.text 
     _t = _t.splitlines()
     data = {}
-    stam = datetime.fromtimestamp(message.date).strftime('%I:%M %p')
-    data['stam']  = stam
-    data['coin']  = _t[0].split(' ')[0]
+    data['coin']  = (_t[0].split(' ')[0]).split('/')[0]
+    data['pair']  = (_t[0].split(' ')[0]).split('/')[1]
     data['type']  = _t[1].split(' ')[0]
-    data['vol']   = _t[2].split('@')[0]
-    data['price'] = _t[2].split('@')[1]
-    data['size']  = _t[3].split(':')[1]
-    data['24h']   = _t[4].split(':')[1]
+    data['vol']   = _t[2].split(' ')[1]
+    data['price'] = _t[2].split(' ')[3]
+    data['size']  = _t[3].split(' ')[2]
+    data['24h']   = _t[4].split(' ')[2]
     data = json.dumps(data)
     req = requests.get(site_url+"wall&data="+data)
     print(req.text)
-  except:
-    print("Error on Wall")
+  except Exception as e:
+    print("Error :: Wall Module ::"+e.message)
 
 
 
@@ -75,15 +69,16 @@ def my_handler(client, message):
     _t = message.text 
     _t = _t.splitlines()
     data = {}
-    data['coin'] = _t[0].split(' ')[2]
-    data['price'] = ' '.join(_t[1].split(' ')[1:3])
-    data['prich'] = (_t[1].split(' ')[3])[1:-1]
-    data['volch'] = (_t[2].split(' ')[3])[1:-1]
-    data['vol'] =' '.join(_t[2].split(' ')[1:3])
+    data['coin']  = (_t[0].split(' ')[2]).split('/')[0]
+    data['pair']  = (_t[0].split(' ')[2]).split('/')[1]
+    data['price'] = _t[1].split(' ')[1]
+    data['vol']   =_t[2].split(' ')[1]
+    data['prich'] = (_t[1].split(' ')[3])[2:-2]
+    data['volch'] = (_t[2].split(' ')[3])[2:-2]
     data = json.dumps(data)
     req = requests.get(site_url+"pump&data="+data)
     print(req.text)
-  except:
-    print("Error on Pump")
+  except Exception as e:
+    print("Error :: Pump Module ::"+e.message)
     
 app.run()
